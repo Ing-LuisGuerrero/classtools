1be04569938a7a2c3eb944131a0e182c127f6750
beyourseff <32877881+beyourseff@users.noreply.github.com>
2022-03-31 18:47:04 +0100

    Update ClassIteratorInterface.php

--- src/Iterator/ClassIteratorInterface.php
+++ src/Iterator/ClassIteratorInterface.php
@@ -1,3 +1,64 @@
+<?php
+/**
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ */
+
+declare(strict_types = 1);
+
+namespace hanneskod\classtools\Iterator;
+
+use hanneskod\classtools\Transformer\Writer;
+
+/**
+ * Iterate over classes found in filesystem
+ *
+ * @author Hannes Forsgård <hannes.forsgard@fripost.org>
+ */
+interface ClassIteratorInterface extends \IteratorAggregate
+{
+    /**
+     * Get syntax errors encountered in source
+     *
+     * @return string[]
+     */
+    public function getErrors(): array;
+
+    /**
+     * Get map of classnames to SplFileInfo objects
+     *
+     * @return SplFileInfo[]
+     */
+    public function getClassMap(): array;
+
+    /**
+     * Enable autoloading for classes found in filesystem
+     */
+    public function enableAutoloading(): void;
+
+    /**
+     * Disable autoloading for classes found in filesystem
+     */
+    public function disableAutoloading(): void;
+
+    /**
+     * Iterator yields classnames as keys and ReflectionClass objects as values
+     */
+    #[\ReturnTypeWillChange]
+    public function getIterator();
+
+    /**
+     * Filter this iterator
+     */
+    public function filter(Filter $filter): Filter;
+
+    /**
+     * Create a new iterator where classes are filtered based on type
+     */
+    public function type(string $typename): Filter;
 
     /**
      * Create a new iterator where classes are filtered based on name
